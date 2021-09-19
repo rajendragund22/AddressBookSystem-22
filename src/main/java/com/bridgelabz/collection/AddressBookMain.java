575b8c6285534f2c60ccab53cba2eef4954dbf31
RajendraGund <rajendragund2015@gmail.com>
2021-09-19 06:10:47 +0530

    Added : Welcome message for the user

---
+++ src/main/java/com/bridgelabz/collection/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.collection;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome into the Program of Address Book System !!");
+    }
+}
