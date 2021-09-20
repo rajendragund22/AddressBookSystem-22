c91a7d9c5b3592500cbba8a28d4dbdad0ea58354
RajendraGund <rajendragund2015@gmail.com>
2021-09-21 02:04:43 +0530

    Added : Program for the Add new contact to address book using console

--- src/main/java/com/bridgelabz/collection/AddressBookMain.java
+++ src/main/java/com/bridgelabz/collection/AddressBookMain.java
@@ -3,15 +3,8 @@ package com.bridgelabz.collection;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome into the Program of Address Book System !!");
-        ContactDetails newContact = new ContactDetails();
-        newContact.setFirstName("Rajendra\n");
-        newContact.setLastName("Gund\n");
-        newContact.setAddressCity("Shirdi\n");
-        newContact.setState("Maharashtra\n");
-        newContact.setEmail("rajendragund@gmail.com");
-        newContact.setZip(414501);
-        newContact.setPhoneNumber( 9604315270L);
-        System.out.println("The Contact is :\n" +newContact);
+        NewAddressBook person = new NewAddressBook();
+        person.addContact();
 
     }
 }
