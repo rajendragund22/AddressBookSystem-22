363f9e84c4e73f74f752d7b9bdd98488d0e9cf4f
RajendraGund <rajendragund2015@gmail.com>
2021-09-26 15:53:08 +0530

    Added : Program for the Search person by the city or the state

---
+++ src/main/java/com/bridgelabz/collection/Sort.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.collection;
+
+import java.util.Comparator;
+
+
+public class Sort {
+    static Comparator<Contacts> compareCity = new Comparator<Contacts>() {
+        public int compare(Contacts one, Contacts two) {
+            return one.getCity().compareTo(two.getCity());
+        }
+    };
+
+}
