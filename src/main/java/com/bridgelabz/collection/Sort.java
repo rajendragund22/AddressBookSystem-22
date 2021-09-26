e83a7b64e6fe9028abc78a1f97064a23d5b4e0da
RajendraGund <rajendragund2015@gmail.com>
2021-09-26 16:03:38 +0530

    Added : Program for the count contact into the address book

--- src/main/java/com/bridgelabz/collection/Sort.java
+++ src/main/java/com/bridgelabz/collection/Sort.java
@@ -2,13 +2,14 @@ package com.bridgelabz.collection;
 
 import java.util.Comparator;
 
+
 public class Sort {
-    static Comparator<Contacts> compareCity = new Comparator<Contacts>() {
+    static Comparator<Contacts> compareCity = new Comparator<>() {
         public int compare(Contacts one, Contacts two) {
             return one.getCity().compareTo(two.getCity());
         }
     };
-    static Comparator<Contacts> compareState = new Comparator<Contacts>() {
+    static Comparator<Contacts> compareState = new Comparator<>() {
         public int compare(Contacts one, Contacts two) {
             return one.getState().compareTo(two.getState());
         }
